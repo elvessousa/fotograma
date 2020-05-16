5d746efba9f926dcfad471d4c8de6407912a65db
Elves Sousa <elves.sousa@gmail.com>
2020-05-16 19:17:56 -0300

    Loading state on feed.

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect, useContext } from 'react';
 import PostItem from '../components/PostItem';
 import { UserContext } from '../App';
 import Message from '../components/Message';
+import Loader from '../components/Loader';
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
   // eslint-disable-next-line
   const { state, dispatch } = useContext(UserContext);
 
   useEffect(() => {
+    setLoading(true);
     fetch('/feed', {
       headers: {
         Authorization: `Bearer ${localStorage.getItem('jwt')}`,
@@ -17,6 +20,7 @@ export default function Home() {
       .then(async (res) => {
         const result = await res.json();
         setData(result.posts);
+        setLoading(false);
       })
       .catch((err) => {
         console.error('error', err);
@@ -43,33 +47,39 @@ export default function Home() {
   };
 
   return (
-    <div className="home">
-      <Message title="All by myseeelf..." text="Don't wanna be..." />
-      {data.map((item) => {
-        return (
-          <div className="post-container" key={item._id}>
-            {item.postedBy._id === state._id && (
-              <button
-                className="delete-post"
-                onClick={() => deletePost(item._id)}
-              >
-                Delete
-              </button>
-            )}
-            <PostItem
-              id={item._id}
-              currentUser={state._id}
-              author={item.postedBy._id}
-              username={item.postedBy.name}
-              title={item.title}
-              text={item.body}
-              image={item.photo}
-              likes={item.likes}
-              allComments={item.comments}
-            />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {!loading ? (
+        <div className="home">
+          <h1>Explore</h1>
+          {data.map((item) => {
+            return (
+              <div className="post-container" key={item._id}>
+                {item.postedBy._id === state._id && (
+                  <button
+                    className="delete-post"
+                    onClick={() => deletePost(item._id)}
+                  >
+                    Delete
+                  </button>
+                )}
+                <PostItem
+                  id={item._id}
+                  currentUser={state._id}
+                  author={item.postedBy._id}
+                  username={item.postedBy.name}
+                  title={item.title}
+                  text={item.body}
+                  image={item.photo}
+                  likes={item.likes}
+                  allComments={item.comments}
+                />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 }
