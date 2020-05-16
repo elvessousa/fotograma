e9ef7250edf9e1afeed58b83f75f53e049ea0cfc
Elves Sousa <elves.sousa@gmail.com>
2020-05-16 19:19:07 -0300

    Order posts by most recent on api.

--- routes/post.js
+++ routes/post.js
@@ -11,6 +11,7 @@ router.get('/all', requireLogin, (req, res) => {
   Post.find()
     .populate('postedBy', '_id name')
     .populate('comments.postedBy', '_id name')
+    .sort('-createdAt')
     .then((posts) => {
       res.json({ posts });
     })
@@ -26,6 +27,7 @@ router.get('/feed', requireLogin, (req, res) => {
   Post.find({ postedBy: { $in: req.user.following } })
     .populate('postedBy', '_id name')
     .populate('comments.postedBy', '_id name')
+    .sort('-createdAt')
     .then((posts) => {
       res.json({ posts });
     })
